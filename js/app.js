8335fcfa7c362b229c8481245cc34e76f91ba94a
Jonathan <jonathanallen105@gmail.com>
2025-08-11 11:15:59 -0400

    updated arrays to const from let

--- js/app.js
+++ js/app.js
@@ -151,7 +151,7 @@ Complete Exercise 11 in the space below:
 */
 
 const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
-let odds = []
+const odds = []
 
 // for (i = 0; i < nums.length; i++) {
 //     if ((nums[i] % 2) > 0) {
@@ -192,9 +192,9 @@ Exercise 12: FizzBuzz with arrays
 Complete Exercise 12 in the space below:
 */
 
-let fizz = []
-let buzz = []
-let fizzbuzz = []
+const fizz = []
+const buzz = []
+const fizzbuzz = []
 
 nums.forEach((arrNum) => {
     if ((arrNum % 3) === 0) {
